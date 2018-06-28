0b8e780bce0e5a9ec655e4823d0bde18fee2a265
vagrant <vagrant@homestead>
2018-06-28 08:53:02 +0000

    Update database configuartion

---
+++ bootstrap/helpers.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zero
+ * Date: 2018/6/28
+ * Time: 16:50
+ * E-mail: zeroibc@qq.com
+ * WebSite: http://www.zeroibc.com
+ */
+
+function get_db_config()
+{
+	if (getenv('IS_IN_HEROKU')) {
+		$url = parse_url(getenv("DATABASE_URL"));
+
+		return $db_config = [
+			'connection' => 'pgsql',
+			'host' => $url["host"],
+			'database'  => substr($url["path"], 1),
+			'username'  => $url["user"],
+			'password'  => $url["pass"],
+		];
+	} else {
+		return $db_config = [
+			'connection' => env('DB_CONNECTION', 'mysql'),
+			'host' => env('DB_HOST', 'localhost'),
+			'database'  => env('DB_DATABASE', 'forge'),
+			'username'  => env('DB_USERNAME', 'forge'),
+			'password'  => env('DB_PASSWORD', ''),
+		];
+	}
+}
