302385961bef51c204b88010f6bd0a87a9b2b36b
vagrant <vagrant@homestead>
2018-06-28 08:27:05 +0000

    first commit

---
+++ Envoy.blade.php
@@ -0,0 +1,7 @@
+
+@servers(['web' => 'vagrant@192.168.10.10'])
+
+@task('deploy')
+    echo 123;
+
+@endtask
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+
+Route::get('/', function () {
+	phpinfo();
+});
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//Route::get('/addRole',function(){
+//	auth()->user()->attachRole(1);
+//});
+//
+//Route::get('/testRole',function(){
+//	dd( auth()->user()->hasRole('owner') );
+//});
